ecd8ff9a271c3319e48d58a9766c2da74a4fdee9
Nguyen Hoa Pham <nhpham27@gmail.com>
2022-05-05 08:22:31 -0500

    Added AdaBoost model

--- adaboost.py
+++ adaboost.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from sklearn.tree import DecisionTreeRegressor
 import matplotlib.pyplot as plt
+
 def adaboost_hyperparameter_tuning(X, y):
     """
         Finding the best depth and # of estimators 
@@ -28,16 +29,16 @@ def adaboost_hyperparameter_tuning(X, y):
         # record the mean error of k-folds
         errors[n] = np.mean(fold_errors)
     parameters['max_depth'] = min(errors, key=errors.get)
-    plt.subplot(1,2,1)
-    plt.plot(errors.keys(), errors.values())
-    plt.title("Absolute error on various maximum depths of decision tree")
-    plt.xlabel("Depth")
-    plt.ylabel("Absolute error")
+#     plt.subplot(1,2,1)
+#     plt.plot(errors.keys(), errors.values())
+#     plt.title("Absolute error on various maximum depths of decision tree")
+#     plt.xlabel("Depth")
+#     plt.ylabel("Absolute error")
     
     names = []
     errors = {}
     # find best # of estimators
-    n_estimators = range(1,100)
+    n_estimators = range(1,101,10)
     for n in n_estimators:
         fold_errors = []
         # split data into k-folds and train the models
@@ -50,11 +51,35 @@ def adaboost_hyperparameter_tuning(X, y):
         # record the mean error of k-folds
         errors[n] = np.mean(fold_errors)
     parameters['n_estimators'] = min(errors, key=errors.get)
-    plt.subplot(1,2,1)
-    plt.plot(errors.keys(), errors.values())
-    plt.title("Absolute error on various number of estimators")
-    plt.xlabel("Number of estimators")
-    plt.ylabel("Absolute error")
-    plt.tight_layout()
-    plt.show()
+#     plt.subplot(1,2,2)
+#     plt.plot(errors.keys(), errors.values())
+#     plt.title("Absolute error on various number of estimators")
+#     plt.xlabel("Number of estimators")
+#     plt.ylabel("Absolute error")
+#     plt.tight_layout()
+#     plt.show()
     return parameters
+    
+def adaboost_analysis():
+    df = pd.read_csv('AllData-Covid-SocioDemographics-Cases-Deaths.csv')
+    df = df.drop(df[df['Death Counts(Per 1000)'] == 0].index)
+    y = df.pop('Death Counts(Per 1000)').to_numpy()
+    df.pop('Zipcode')
+    X = df[['Some College(%)', 'Asian(%)', 'Same House Prev Year(%)', 'Moved Since Prev Year(%)', 
+            'High School(%)', 'Median housing value', 'No Degree(%)', 'Below poverty line(%)',
+           'Married (%)', 'Single (%)', 'Number of housing units', 'Number of households', 'Population',
+           'Owner Occupied (%)', 'Renter Occupied (%)', 'White(%)', 'Hispanic(%)', 'Mean travel time to work (Minutes)',
+           'Other (%)', 'Drove Alone (%)', 'Foriegn Born Population(%)', 'Public Transit (%)']].to_numpy()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    # baseline model
+    median_deaths = np.median(y_train)
+    baseline_pred = np.repeat(median_deaths, len(y_test))
+    print("Average absolute baseline error (deaths per 1000) = ", round(np.mean(abs(baseline_pred - y_test)), 2))
+    
+    # AdaBoost model
+    params = adaboost_hyperparameter_tuning(X, y)
+    mdl = AdaBoostRegressor(base_estimator=DecisionTreeRegressor(max_depth=params['max_depth']), 
+                           random_state=0,
+                           n_estimators=params['n_estimators'])
+    mdl.fit(X_train, y_train)
+    print("Average absolute model error (deaths per 1000) = ", round(np.mean(abs(mdl.predict(X_test) - y_test)),2))
