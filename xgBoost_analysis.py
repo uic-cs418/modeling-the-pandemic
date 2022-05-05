0204329dffa60df95e6fe64d621c30a6c2ac4a76
Gautam Kushwah <gautamkushwah@gmail.com>
2022-05-05 09:16:12 -0500

    updating

--- xgBoost_analysis.py
+++ xgBoost_analysis.py
@@ -18,15 +18,27 @@ warnings.filterwarnings('ignore')
 
 
 def xgBoost_analysis():
-    df = pd.read_csv("AllData-Covid-SocioDemographics-Cases-Deaths.csv")
-    y = df['Death Counts(Per 1000)']
-    X = df.drop('Death Counts(Per 1000)', 1)
-    X = X.drop('Death Counts', 1)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-    
+    data = pd.read_csv("AllData-Covid-SocioDemographics-Cases-Deaths.csv")
+    cormat = data.corr().abs()
+    cormat = cormat[cormat>0.1]
+    feature_importance = round(cormat,2).dropna(subset=['Death Counts(Per 1000)'])['Death Counts(Per 1000)'].to_frame().reset_index()
+    feature_importance = feature_importance[:-2]
+    important_features = list(feature_importance['index'])
+    data_input = data.iloc[:,:-2]
+    data_input = data_input.set_index('Zipcode')
+    data_input = data_input[important_features]
+
+    X_train, X_test, y_train, y_test  = trim_and_split(data_input)
+    X_train = X_train.drop('Death Counts', 1)
+    X_test = X_test.drop('Death Counts', 1)
     tuned_model = XGBRegressor(colsample_bytree=0.9, gamma=0.0, max_depth=5, n_estimators=80,
              objective='reg:squarederror', subsample=0.9)
     tuned_model.fit(X_train, y_train)
+    y_pred_updated = tuned_model.predict(X_test)
+    absolute_error = abs(y_pred_updated - y_test)
+    abs_error = round(np.mean(absolute_error), 2)
+    MSE = mean_squared_error(y_test, y_pred_updated)
+    R2_score = r2_score(y_test, y_pred_updated)
 
     train_error = abs((tuned_model.predict(X_train)) - (y_train))
     test_error = abs((tuned_model.predict(X_test)) - (y_test))
